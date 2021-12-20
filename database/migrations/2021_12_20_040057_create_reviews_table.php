cf7ca4b963a474b7cf7973f574a550dfff592e0c
paperplane771 <speedfoxdev@gmail.com>
2021-12-20 08:06:15 +0400

    Миграции
    Создал миграции

--- database/migrations/2021_12_20_040057_create_reviews_table.php
+++ database/migrations/2021_12_20_040057_create_reviews_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateUsersTable extends Migration
+class CreateReviewsTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,14 +13,13 @@ class CreateUsersTable extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('reviews', function (Blueprint $table) {
             $table->id();
-            $table->string('fio');
-            $table->char('tel');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->rememberToken();
+            $table->foreignIdFor('City','id');
+            $table->string('title');
+            $table->string('text');
+            $table->enum('rating',[1,2,3,4,5]);
+            $table->foreignIdFor('User','id');
             $table->timestamps();
         });
     }
@@ -32,6 +31,6 @@ class CreateUsersTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('reviews');
     }
 }
